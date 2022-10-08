f5156fc7697a27e7ca3985b865bd46f27dfd0b95
younes <yoneskhosrai7676@gmail.com>
2022-10-08 05:54:49 -0600

    shopping-cart

---
+++ src/components/Store.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+// Components
+import Product from './shared/Product';
+import Loader from './shared/Loader';
+//redux
+import { fetchProducts } from '../redux/products/productsAction';
+
+// Style
+import styles from "./Store.module.css";
+
+const Store = () => {
+
+const dispatch = useDispatch()
+const productsState=useSelector(state=>state.productsState)
+useEffect(() => {
+    if (!productsState.products.length) dispatch(fetchProducts())
+}, [])
+    return (
+        <div className={styles.container} >
+            {
+                productsState.loading?
+                <Loader/> :
+                   productsState.error?<p>SomeThings Went Wrong</p>:
+                   productsState.products.map(product=><Product
+                   key={product.id}
+                   productData={product}
+                   />)
+            }
+        </div>
+    );
+};
+
+export default Store;
